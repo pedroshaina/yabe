fd6b93ceb46178b2084696069403e2146f1b69a0
pedroshaina <pedroshaina_@hotmail.com>
2021-10-24 12:13:22 -0300

    implementing transactions api

--- explorer-backend/explorer-api/src/modules/controller/transactions/index.js
+++ explorer-backend/explorer-api/src/modules/controller/transactions/index.js
@@ -1,10 +1,33 @@
 module.exports = (transactionService) => {
     const listTransactionsByBlockHash = (req, res) => {
 
+        if (!req.query.blockHash || !req.query.page || !req.query.pageSize) {
+            res.status(400).json({ error: 'You must provide the following query params: blockHash, page, pageSize' })
+        }
+
+        const {
+            blockHash,
+            page,
+            pageSize
+        } = req.query
+
+        const blockTransactions = await transactionService.listTransactionsByBlockHash(blockHash, page, pageSize)
+
+        return res.json(blockTransactions)
     }
 
     const getTransactionByTxId = (req, res) => {
+        if (!req.params.txid) {
+            res.status(400).json({ error: 'You must provide the following path param: txid' })
+        }
 
+        const {
+            txid
+        } = req.params
+        
+        const transaction = await transactionService.getTransactionByTxId(txid)
+
+        return res.json(transaction)
     }
 
     return {
--- explorer-backend/explorer-api/src/modules/service/transaction/index.js
+++ explorer-backend/explorer-api/src/modules/service/transaction/index.js
@@ -1,3 +1,14 @@
-module.exports = (trasactionDao) => {
-    
+module.exports = (transactionDao) => {
+    const listTransactionsByBlockHash = async (blockHash, page, pageSize) => {
+        return await transactionDao.loadMinifiedTransactionsByBlockHash(blockHash, page, pageSize)
+    }
+
+    const getTransactionByTxId = async (txid) => {
+        return await transactionDao.loadFullTransacionByTxid(txid)
+    }
+
+    return {
+        listTransactionsByBlockHash,
+        getTransactionByTxId
+    }
 }
